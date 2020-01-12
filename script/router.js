73d925ce8e703be86842dcf205b87d61e09a134b
pwoicik <patrykwoicik@gmail.com>
2020-01-12 19:28:30 +0100

    restructured codebase

--- script/router.js
+++ script/router.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import {Home} from "./pages/home.js";
-import {About} from "./pages/about.js";
-import {Charts} from "./pages/charts.js";
-import {Search} from "./pages/search.js";
-import {Song} from "./pages/song.js";
-import {NavBar} from "./components/navBar.js";
-import {Error404} from "./pages/error404.js";
+import {Home} from "../pages/home.js";
+import {About} from "../pages/about.js";
+import {Charts} from "../pages/charts.js";
+import {Search} from "../pages/search.js";
+import {Song} from "../pages/song.js";
+import {NavBar} from "../components/navBar.js";
+import {Error404} from "../pages/error404.js";
 
 const routes = {
     "/": Home,
@@ -16,7 +16,7 @@ const routes = {
     "/song/:id": Song,
 };
 
-const router = async function () {
+export const router = async function () {
     const $navbarCollapse = $("#navbarCollapse");
     const $overlay = $("#overlay");
 
@@ -37,9 +37,3 @@ const router = async function () {
     $overlay.empty();
     $overlay.append(await page.render(request.args));
 };
-
-export const LASTFM_API_KEY = "3304c4acb8acdc4e5b97eb12653b0024";
-export const GENIUS_API_KEY = "xnRq3l0P2GQojPgTW6G40UqfPiDIhWpwPhZqDSfA4o8KeE6ylg_oN2L6nN2-0JrF";
-
-window.addEventListener("hashchange", router);
-window.addEventListener("load", router);
