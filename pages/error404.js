af050546c320afe3474379928dbb22de9ef2cf09
krucan-a <andrzejkrucan@gmail.com>
2020-01-12 21:12:58 +0100

    error 404

--- pages/error404.js
+++ pages/error404.js
@@ -2,6 +2,6 @@
 
 export const Error404 = {
     async render() {
-        return $("<h1>PAGE NOT FOUND!</h1>");
+        return $("<h1>PAGE NOT FOUND!</h1><img src='../assets/banner_error_404_3.jpg' class='responsive'></img>");
     },
 };
