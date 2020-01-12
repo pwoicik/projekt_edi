f5b1bf87f55b1d6a4bc9f4ce258e12943101bc25
krucan-a <andrzejkrucan@gmail.com>
2020-01-12 23:48:08 +0100

    Final one

--- pages/error404.js
+++ pages/error404.js
@@ -2,7 +2,7 @@
 
 export const Error404 = {
     async render() {
-        return $(`<h1 class='display-4 text-white' >PAGE NOT FOUND!</h1>
-        <img src='../assets/penguins404.jpg' class='responsive'></img>`);
+        return `<h1 class='display-4 text-white'>PAGE NOT FOUND!</h1>
+        <img src="assets/penguins404.jpg" class="img-fluid" alt="Responsive image">`;
     },
 };
