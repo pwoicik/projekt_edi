e929c073516ff8fbcf8ebc9dbed2f36b98bb8ab4
krucan-a <andrzejkrucan@gmail.com>
2020-01-25 23:43:57 +0100

    about page

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@
 
 export const About = {
     async render() {
-        return "<h1>ABOUT</h1>";
+        return `<h1 class='display-4 text-white' style="text-align:center">AUTORZY</h1>
+                <h1 class='display-3 text-white' style="text-align:center">Patryk Wójcik<br>Kamil Krzych<br>Andrzej Krucan</span>`;
     },
 };
